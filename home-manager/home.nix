c2abf69e4a6f340d5cadc920d13250df88ab3278
MdotAmaan <mdotamaan@protonmail.com>
2025-01-22 14:46:36 -0500

    Try out VimJoyer's lf config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -165,7 +165,7 @@
             "gapsOuterTop" = 9;
             "manualResizeStep" = 300;
 
-            "windowRules" = ''               
+            "windowRules" = ''                 
               [{        
                 "class": "ksmserver-logout-greeter",
                 "tile": false
@@ -307,30 +307,69 @@
 
     lf = {
       enable = true;
-
       commands = {
         dragon-out = ''%${pkgs.xdragon}/bin/xdragon -a -x "$fx"'';
         editor-open = ''$$EDITOR $f'';
         mkdir = ''
           ''${{
-              printf "Directory Name: "
-              read DIR
-              mkdir $DIR
-             }}
+            printf "Directory Name: "
+            read DIR
+            mkdir $DIR
+          }}
         '';
       };
 
       keybindings = {
-        D = "trash";
-        "<delete>" = "trash";
-        gh = "cd ~";
-        r = "push :rename<space>";
-        nd = "push :mkdir<space>";
-        nf = " push :mkfile<space>";
-        o = "&mimeopen $f";
-        O = " $mimeopen --ask $f";
-        xt = "extract";
+        "\\\"" = "";
+        o = "";
+        c = "mkdir";
+        "." = "set hidden!";
+        "`" = "mark-load";
+        "\\'" = "mark-load";
+        "<enter>" = "open";
+
+        do = "dragon-out";
+
+        "g~" = "cd";
+        gh = "cd";
+        "g/" = "/";
+
+        ee = "editor-open";
+        V = ''$${pkgs.bat}/bin/bat --paging=always --theme=gruvbox "$f"'';
+
+        # ...
       };
+
+      settings = {
+        preview = true;
+        hidden = true;
+        drawbox = true;
+        icons = true;
+        ignorecase = true;
+      };
+
+      extraConfig = let
+        previewer = pkgs.writeShellScriptBin "pv.sh" ''
+          file=$1
+          w=$2
+          h=$3
+          x=$4
+          y=$5
+
+          if [[ "$( ${pkgs.file}/bin/file -Lb --mime-type "$file")" =~ ^image ]]; then
+              ${pkgs.kitty}/bin/kitty +kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$file" < /dev/null > /dev/tty
+              exit 1
+          fi
+
+          ${pkgs.pistol}/bin/pistol "$file"
+        '';
+        cleaner = pkgs.writeShellScriptBin "clean.sh" ''
+          ${pkgs.kitty}/bin/kitty +kitten icat --clear --stdin no --silent --transfer-mode file < /dev/null > /dev/tty
+        '';
+      in ''
+        set cleaner ${cleaner}/bin/clean.sh
+        set previewer ${previewer}/bin/pv.sh
+      '';
     };
   };
 
