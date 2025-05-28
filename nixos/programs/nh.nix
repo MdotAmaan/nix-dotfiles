a47405b8c1c1f7d1f92ac890787ea033d3fd24ff
MdotAmaan <mdotamaan@protonmail.com>
2025-05-27 22:48:07 -0400

    Fix nh path

--- nixos/programs/nh.nix
+++ nixos/programs/nh.nix
@@ -3,6 +3,6 @@
     enable = true;
     clean.enable = true;
     clean.extraArgs = "--keep-since 4d --keep 3";
-    flake = "/home/mdot/nix-dotfiles/.#${config.host}";
+    flake = "/home/mdot/nix-dotfiles/";
   };
 }
