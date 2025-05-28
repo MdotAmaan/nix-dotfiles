15695f2355118896db7a303875378b4ee08edc10
MdotAmaan <mdotamaan@protonmail.com>
2025-05-27 22:39:36 -0400

    Revert low latency audio config

--- hosts/dotPC/configuration.nix
+++ hosts/dotPC/configuration.nix
@@ -92,35 +92,7 @@
         alsa.enable = true;
         alsa.support32Bit = true;
         pulse.enable = true;
-
-        extraConfig.pipewire."92-low-latency" = {
-          "context.properties" = {
-            "default.clock.rate" = 48000;
-            "default.clock.quantum" = 32;
-            "default.clock.min-quantum" = 32;
-            "default.clock.max-quantum" = 32;
-          };
-        };
-        extraConfig.pipewire-pulse."92-low-latency" = {
-          context.modules = [
-            {
-              name = "libpipewire-module-protocol-pulse";
-              args.pulse = {
-                min.req = "32/48000";
-                default.req = "32/48000";
-                max.req = "32/48000";
-                min.quantum = "32/48000";
-                max.quantum = "32/48000";
-              };
-            }
-          ];
-          stream.properties = {
-            node.latency = "32/48000";
-            resample.quality = 1;
-          };
-        };
       };
-
       openssh.enable = true;
       resolved.enable = true;
 
