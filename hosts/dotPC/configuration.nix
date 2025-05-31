fbdc8e92ca8c6c643eb0b60c72ccdf21a0d4204f
MdotAmaan <mdotamaan@protonmail.com>
2025-05-31 15:45:26 -0400

    Force older kernel until AMDGPU crash is fixed

--- hosts/dotPC/configuration.nix
+++ hosts/dotPC/configuration.nix
@@ -19,6 +19,18 @@
         efi.canTouchEfiVariables = true;
         timeout = 0;
       };
+
+      # Force older kernel to prevent AMDGPU crash. Remove once fix is applied uupstream
+      kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_6_14.override {
+        argsOverride = rec {
+          src = pkgs.fetchurl {
+            url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
+            sha256 = "sha256-IYF/GZjiIw+B9+T2Bfpv3LBA4U+ifZnCfdsWznSXl6k=";
+          };
+          version = "6.14.6";
+          modDirVersion = "6.14.6";
+        };
+      });
     };
 
     virtualisation.docker.enable = true;
